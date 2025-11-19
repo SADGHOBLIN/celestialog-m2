7e085cf0abd370ce6386cf59a93a530c84e737e3
sadghoblin <171681941+SADGHOBLIN@users.noreply.github.com>
2025-11-19 20:24:48 +0000

    Add DOM rendering of moon data (closes #11)

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ async function saveAstronomyData() {
             timestamp: Date.now()
         };
         localStorage.setItem("astronomyData", JSON.stringify(payload));
-        console.log("Data successfully saved to local stoage");
+        console.log("Data successfully saved to local storage");
         return payload;
     } else {
         const errorMessage = data.message || data.error || "Unknown API error";
@@ -39,11 +39,33 @@ function isDataFresh(payload) {
     const currentDate = new Date().toDateString();
 
     if (age <= expiryTime && dataDate === currentDate) {
-        console.log("data is fresh");
         return true;
     } else {
-        console.log("data is stale");
         return false;
+    }
+}
+
+// Convert moon phrase from API data to readable format
+function formatMoonPhase(currentPhase) {
+    return currentPhase
+        .toLowerCase()
+        .split("_")
+        .map(word => word[0].toUpperCase() + word.slice(1))
+        .join(" ");
+}
+
+// Check moon visibility based on moonrise and moonset data
+function checkMoonVisibility(today, moonrise, moonset) {
+    const now = today.getHours() * 100 + today.getMinutes();
+    const [riseHour, riseMin] = moonrise.split(":").map(Number);
+    const moonriseNum = riseHour * 100 + riseMin;
+    const [setHour, setMin] = moonset.split(":").map(Number);
+    const moonsetNum = setHour * 100 + setMin;
+
+    if (moonriseNum < moonsetNum) {
+        return now >= moonriseNum && now <= moonsetNum;
+    } else {
+        return ((now >= moonriseNum && now <= 2359) || (now >= 0 && now <= moonsetNum ));
     }
 }
 
@@ -56,6 +78,31 @@ async function displayMoonData() {
         payload = await saveAstronomyData();
     }
 
+    // Moon info variables
+    const today = new Date(payload.timestamp);
     const moonData = payload.moonData;
-    console.log("Data ready for use:", moonData);
+    const moonPhase = formatMoonPhase(moonData.astronomy.moon_phase);
+    const moonrise = moonData.astronomy.moonrise;
+    const moonset = moonData.astronomy.moonset;
+
+    // Display current moon phase
+    const currentMoonPhase = document.getElementById("moon-phase-data");
+    currentMoonPhase.innerText = moonPhase;
+
+    // Display current date
+    const currentDate = document.getElementById("current-date-data");
+    currentDate.innerText = today.toDateString();
+
+    // Display moon visibility
+    const moonVisibility = document.getElementById("moon-visibility-data");
+    const isVisible = checkMoonVisibility(today, moonrise, moonset);
+    if (isVisible) {
+        moonVisibility.innerHTML = `<p id="moon-visibility">VISIBLE<br>${moonrise} --- ${moonset}</p>`;
+    } else {
+        moonVisibility.innerHTML = `<p id="moon-visibility">Moonrise:<br>${moonrise}</p>`;
+    }
+
+    console.log(moonData);
 }
+
+displayMoonData();
