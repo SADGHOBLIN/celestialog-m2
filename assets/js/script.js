fc8b9ecb13fbbd42ac3d2e6d2ba535ca9a4ba7ec
sadghoblin <171681941+SADGHOBLIN@users.noreply.github.com>
2025-11-19 14:20:50 +0000

    Save timestamp of moon data to local storage with capability to load

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Astronomy API from ipgeolocation
 // GET astronomy data from API and save to local storage
-async function getMoonData() {
+async function saveAstronomyData() {
     const API_KEY = "1cd00c803c544f90b75357b117b4c27a";
     const API_URL = "https://api.ipgeolocation.io/v2/astronomy";
     let city = "Leeds%2C%20UK";
@@ -10,12 +10,47 @@ async function getMoonData() {
     const data = await response.json();
     
     if (response.ok) {
-        localStorage.setItem("astronomyData", JSON.stringify(data));
-        return data;
+        const payload = {
+            moonData: data,
+            timestamp: Date.now()
+        };
+        localStorage.setItem("astronomyData", JSON.stringify(payload));
+        return payload;
     } else {
         const errorMessage = data.message || data.error || "Unknown API error";
         throw new Error(errorMessage);
     }
 }
 
-getMoonData();
+// Load API data from local storage or fetch new data if none stored
+async function getMoonData() {
+    const savedData = localStorage.getItem("astronomyData");
+
+    return savedData
+    ? JSON.parse(savedData)
+    : await saveAstronomyData();
+}
+
+getMoonData().then(savedData => {
+    console.log(savedData.moonData);
+    console.log(savedData.timestamp);
+    console.log(savedData.moonData.astronomy);
+});
+
+
+// async function displayMoonData() {
+//     getMoonData();
+//     console.log(moonData.astronomy);
+// }
+
+// on refresh:
+// Is data available?
+
+// no > GET NEW DATA > data
+//      run get function, return data
+
+// yes > IS DATA FRESH? > no ^ > yes > data
+//      data.time IF statement > data
+//      GET NEW DATA > data
+
+// FRESH DATA > use
